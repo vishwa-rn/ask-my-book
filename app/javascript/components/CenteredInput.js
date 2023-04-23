e739ace4cae994407d420e2baca73c69c4607aa2
Vishwa Rayavarapu <rvish@Vishwas-MacBook-Pro.local>
2023-04-24 01:53:24 +0530

    Minor change: Updated the placeholder in the ask question input text box.

--- app/javascript/components/CenteredInput.js
+++ app/javascript/components/CenteredInput.js
@@ -5,7 +5,7 @@ const CenteredInput = ({ value, onChange }) => (
     value={value}
     onChange={onChange}
     className="block w-full max-w-2xl mx-auto my-2 text-lg p-2 text-white bg-gray-700 border border-gray-600 rounded-md shadow-inner focus:outline-none focus:border-gray-500 xs:mx-4 sm:mx-4"
-    placeholder="What is Atomic Habits about?"
+    placeholder="What is Origin of species about?"
     rows="2"
   />
 );
